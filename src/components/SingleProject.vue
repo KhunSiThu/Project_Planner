87ac17661dad4d0d94e53c8f30e9d6c3bb98f1c5
KhunSiThu <khunsithu65@gmail.com>
2024-07-27 04:48:28 +0630

    commit 1

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="project">
+        <div class="flexing">
+            <div><h3 @click="showDetail = !showDetail">{{project.title}}</h3></div>
+            <div class="icon-con">
+                <i class="fa-solid fa-trash"></i>
+                <i class="fa-solid fa-pen-to-square"></i>
+                <i class="fa-regular fa-square-check"></i>
+            </div>
+        </div>
+        
+        <p v-if="showDetail">{{project.detail}}</p>
+       
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['project'],
+
+    data(){
+        return{
+            showDetail : false
+        }
+    }
+}
+</script>
+
+<style>
+    .project {
+        padding: 20px;
+        background-color: #f2f2f2;
+        margin: 10px;
+        border-left: 5px solid red;
+        border-radius: 5px;
+    }
+
+    h3 {
+        color: indigo;
+        cursor: pointer;
+    }
+
+    .flexing {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .flexing i {
+            margin-left: 15px;
+            color: #7777;
+        }
+    
+    .flexing i:hover {
+        transition: .5s;
+        color: black;
+        cursor: pointer;
+    }
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project="project"></SingleProject>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import SingleProject from '../components/SingleProject'
+export default {
+  name: 'HomeView',
+  components: {
+    SingleProject,
+
+  },
+
+  data(){
+    return {
+      projects: []
+    }
+  },
+
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      this.projects = data;
+    })
+    .catch(() => {
+
+    })
+  }
+}
+</script>
